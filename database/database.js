eafddb66993de0efe4be85e229778a36c280bde1
André Carvalho <fandrecarvalho05@gmail.com>
2021-11-03 11:31:52 +0000

    conexão à base de dados

--- database/database.js
+++ database/database.js
@@ -1,1 +1,9 @@
 const Sequelize = require('sequelize');
+
+const connection = new Sequelize ('social', 'root','', {
+    host:'localhost',
+    dialect: 'mysql',
+    timezone: "+01:00"
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const connection = require("./database/database.js");
+
+//database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com a base de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    });
 
 // Instrução para o Express usar o EJS como View engine
 app.set('view engine','ejs');
